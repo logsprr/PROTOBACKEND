c97b438ee273b03bfa89dfade8d8a561d0a46d5d
Gabriel Gomes <corinthiaswwyy@gmail.com>
2020-03-28 13:14:19 -0300

    normal firebase

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const cors = require('cors');
 const app = express();
 const routes = require('./routes/routes');
 const firebase = require('firebase');
-const settingFirebase = require('./json/firebase.json')
-const config = settingFirebase;
+const config = {
+    apiKey: "AIzaSyDb-8HAjGxahK-dq6mokIVa8Hb2J31rwnY",
+    authDomain: "nativetodo-12412.firebaseapp.com",
+    databaseURL: "https://nativetodo-12412.firebaseio.com",
+    projectId: "nativetodo-12412",
+    storageBucket: "nativetodo-12412.appspot.com",
+    messagingSenderId: "204666161034",
+    appId: "1:204666161034:web:d103f48b20c339b4da6a73"
+}
 firebase.initializeApp(config);
 app.use(cors());
 app.use(bodyParser.json());
